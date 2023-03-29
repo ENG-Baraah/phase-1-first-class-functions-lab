9f14fc25404cdb4bd271db98c94299306733da14
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2023-03-30 00:41:03 +0300

    Completed  First-Class Functions Lab Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(drivers.length - 2, drivers.length);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(factor) {
+  return function (value) {
+    return factor * value;
+  };
+}
+
+const fareDoubler = function (value) {
+  return createFareMultiplier(2)(value);
+};
+
+const fareTripler = function (value) {
+  return createFareMultiplier(3)(value);
+};
+
+function selectDifferentDrivers(arr, returnFirstTwoDrivers) {
+  return returnFirstTwoDrivers(arr);
+}
